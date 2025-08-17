5afb045bf07407c02cd82a6e0fc1c5d067e30a5d
Abdullah <am7071363@gmail.com>
2025-08-17 20:39:02 +0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
+// frontend/src/api/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://spare-backend-production.up.railway.app/api",
 });
 
 export default instance;
